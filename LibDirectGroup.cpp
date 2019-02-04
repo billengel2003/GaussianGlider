c4f3b65a54c2847437c1b8428b3a5ead32339199
William Engel <billengel2003@yahoo.com>
2019-02-04 13:50:24 -0500

    Added terminal Command

--- LibDirectGroup.cpp
+++ LibDirectGroup.cpp
@@ -1,24 +1,24 @@
 #include "directgroup.h"
 
 //g++ -c -fPIC -std=c++11 directgroup.h staticgroup.h smtptr.h directgroup.cpp staticgroup.cpp LibDirectGroup.cpp
-// g++ -shared -Wl,-soname,libfoo.so -o libfoo.so overgroup.o undergroup.o ExternC.o
+ //g++ -shared -Wl,-soname,libfoo.so -o libfoo.so directgroup.o groupt.o LibDirectGroup.o staticgroup.o
 
 extern "C"{
-	DirectGroup * DirectGroup_New(const size_t size, const size_t D){		
-		DirectGroup * DG = new DirectGroup(size, D); 
+	DirectGroup * DirectGroup_New(const size_t size, const size_t D){
+		DirectGroup * DG = new DirectGroup(size, D);
 		return DG;
 	}
 	void TakeIn(DirectGroup * dg, const char * url_text){
-		dg->TakeInHTML(url_text); 
+		dg->TakeInHTML(url_text);
 	}
 	void MyFamily(DirectGroup * dg, const char * child, const int greater_than){
-		dg->MyFamily(child, greater_than); 
+		dg->MyFamily(child, greater_than);
 	}
 	void End(DirectGroup * dg){
 		delete dg;
 	}
 	void Forget(DirectGroup * dg){
-		dg->Forget(); 	
+		dg->Forget();
 	}
 }
 
